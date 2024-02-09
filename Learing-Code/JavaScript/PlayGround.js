6649d9e46cf681c17b93a90f96f46f301ed749b3
mdtanvirahamedshanto <mdtanvirahamdsanto987@gmail.com>
2024-02-09 22:33:02 +0600

    Problem solve

--- Learing-Code/JavaScript/PlayGround.js
+++ Learing-Code/JavaScript/PlayGround.js
@@ -1,20 +1,4 @@
-const user = {
-    name :"Shanto",
-    age: 21,
-    education:{
-        degre: "HSC",
-        school: {
-            hsc: "dhulasar",
-            ds:{
-                // d:"clg",
-        }
-    }
-}
-}
+let number = [100, 2, 3, 4, 5];
+const maxValue = Math.min(...number);
 
-const {education:{school:{ds:{d} = {}}}} = user;
-
-// const data = user?.education?.school?.ds?.d;
-console.log(d);
-
-
+console.log(maxValue);
